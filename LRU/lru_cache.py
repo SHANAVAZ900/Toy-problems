2d68606a4bacc6d5c1b156c58c10bea13002735c
zamindar-786 <zamindar900@gmail.com>
2020-05-29 01:11:26 +0530

    added the logic to the put method

--- LRU/lru_cache.py
+++ LRU/lru_cache.py
@@ -7,6 +7,19 @@ class LRU_Cache:
 # required methods:   put, get and get_cache
 
     def put(self, key):
+        if (len(self.lru) < self.size):
+            if key in self.lru:
+                self.lru.remove(key)
+                self.lru.append(key)
+                self.cache[key] = str(key)
+            else:
+                self.lru.append(key)
+                self.cache[key] = str(key)
+        else:
+            x = self.lru.pop(0)
+            self.lru.append(key)
+            del self.cache[x]
+            self.cache[key] = str(key)
 
     def get(self, key):
 
